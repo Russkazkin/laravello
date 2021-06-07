95b3e5e4566c195c7629c2613c037cad361e5b42
Russkazkin <ruslan@skazkin.su>
2021-06-07 17:21:33 +0300

    Add model relationships

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\CardList;
 
 class Board extends Model
 {
     use HasFactory;
+
+    public function lists(): HasMany
+    {
+        return $this->hasMany(CardList::class);
+    }
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
 }
