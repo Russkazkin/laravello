518b9fbaba6e07b4891abee4481e98cb518ac868
Russkazkin <ruslan@skazkin.su>
2021-07-21 06:40:56 +0300

    Update cache after card add

--- resources/js/components/CardAddButton.vue
+++ resources/js/components/CardAddButton.vue
@@ -3,7 +3,8 @@
 </template>
 
 <script>
-import CardAdd from '../graphql/CardAdd.gql'
+import CardAdd from '../graphql/CardAdd.gql';
+import BoardQuery from '../graphql/BoardWIthListsAndCards.gql';
 
 export default {
   name: "CardAddButton",
@@ -15,6 +16,14 @@ export default {
           title: 'Added from Vue',
           listId: 1,
           order: 1,
+        },
+        update(store, {data: {cardAdd}}) {
+          const data = store.readQuery({
+            query: BoardQuery,
+            variables: {id: 1}
+          });
+          data.board.lists.find(list => list.id = 1).cards.push(cardAdd);
+          store.writeQuery({ query: BoardQuery, data });
         }
       });
     }
