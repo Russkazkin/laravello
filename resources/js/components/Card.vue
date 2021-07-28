499b3a58f5fddeb5f3a6624306911c2b4cb7818a
Russkazkin <ruslan@skazkin.su>
2021-07-28 07:17:19 +0300

    Add card buttons

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="bg-white rounded-sm p-2 cursor-pointer text-sm hover:bg-gray-100 mb-2 shadow-card">{{ card.title }}</div>
+  <div class="group bg-white rounded-sm p-2 cursor-pointer text-sm hover:bg-gray-100 mb-2 shadow-card flex justify-between">
+    <div>{{ card.title }}</div>
+    <div class="flex font-bold opacity-0 group-hover:opacity-100 transition-opacity ease-in-out">
+      <div class="text-gray-400 mr-2 p-1 hover:text-purple-700">E</div>
+      <div class="text-gray-400 p-1 hover:text-red-700">D</div>
+    </div>
+  </div>
 </template>
 
 <script>
