71c9fc43a09b711f5172cd0010d8b0c21b4af68a
Russkazkin <ruslan@skazkin.su>
2021-08-15 06:48:55 +0300

    Add base markup for login page

--- resources/js/Login.vue
+++ resources/js/Login.vue
@@ -1,5 +1,11 @@
 <template>
-<div>Login</div>
+<div class="bg-white sm:bg-gray-100 h-full flex justify-center">
+  <div class="container mt-2 sm:mt-10 flex flex-col items-center">
+    <div class="text-3xl text-blue-700 font-bold mb-10">
+      <span>Laravello</span>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -8,6 +14,12 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="sass" scoped>
+.container
+  width: 300px
+  max-width: 300px
+@media (min-width: 640px)
+  .container
+    width: 400px
+    max-width: 400px
 </style>
