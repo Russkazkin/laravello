946a33070d699a27a96cc0284487cd822a8490d4
Russkazkin <ruslan@skazkin.su>
2021-09-28 06:40:10 +0300

    Setup csrf-token

--- resources/js/apollo.config.js
+++ resources/js/apollo.config.js
@@ -5,6 +5,10 @@ import VueApollo from "vue-apollo";
 Vue.use(VueApollo);
 const apolloClient = new ApolloClient({
   uri: '/graphql',
+  headers: {
+    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content
+  },
+  credentials: 'include',
 });
 
 export default new VueApollo({
