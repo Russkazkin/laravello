7eab96063fb0338762c37a64e152438c76a71ea9
Russkazkin <russkazkin@gmail.com>
2021-10-04 07:49:54 +0300

    Add registration logic in vue component

--- resources/js/Register.vue
+++ resources/js/Register.vue
@@ -5,13 +5,17 @@
         <span>Laravello</span>
       </div>
       <div class="w-full sm:shadow-xl sm:bg-white sm:py-8 sm:px-12">
+        <div v-if="errors.length" class="p-2 bg-red-600 text-gray-100 rounded-sm mb-6 text-sm text-center">
+          <div v-for="(error, index) in errors" :key="index">{{ error.message }}</div>
+        </div>
         <div class="w-full text-center text-gray-600 font-bold mb-8">Signup to your account</div>
-        <form>
+        <form @submit.prevent="register">
           <div class="w-full mb-4">
             <input
               type="text"
               class="rounded-sm px-4 py-2 outline-none focus:outline-none border-gray-400 bg-gray-100 border-solid border-2 w-full text-sm"
               placeholder="Enter email"
+              v-model="email"
             >
           </div>
           <div class="w-full mb-4">
@@ -19,6 +23,7 @@
               type="text"
               class="rounded-sm px-4 py-2 outline-none focus:outline-none border-gray-400 bg-gray-100 border-solid border-2 w-full text-sm"
               placeholder="Enter full name"
+              v-model="name"
             >
           </div>
           <div class="w-full mb-4">
@@ -26,6 +31,7 @@
               type="password"
               class="rounded-sm px-4 py-2 outline-none focus:outline-none border-gray-400 bg-gray-100 border-solid border-2 w-full text-sm"
               placeholder="Enter password"
+              v-model="password"
             >
           </div>
           <div class="w-full mb-6">
@@ -42,8 +48,37 @@
 </template>
 
 <script>
+import Register from "./graphql/Register.gql";
+import {gqlErrors} from "./utils";
+
 export default {
-  name: "Register"
+  name: "Register",
+  data() {
+    return {
+      email: null,
+      name: null,
+      password: null,
+      errors: [],
+    }
+  },
+  methods: {
+    async register() {
+      this.errors = [];
+      try {
+        const response = (await this.$apollo.mutate({
+          mutation: Register,
+          variables: {
+            name: this.name,
+            email: this.email,
+            password: this.password,
+          }
+        }));
+        await this.$router.push({name: "board"});
+      } catch (error) {
+        this.errors = gqlErrors(error);
+      }
+    }
+  }
 }
 </script>
 
