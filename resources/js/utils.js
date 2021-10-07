4a73908046100785a2e2c175039111bd8f6058b9
Russkazkin <russkazkin@gmail.com>
2021-10-07 07:28:13 +0300

    Add validation errors output

--- resources/js/utils.js
+++ resources/js/utils.js
@@ -1,8 +1,14 @@
 export function gqlErrors(error) {
   const hasInternal = errors => errors.some(e => e.internal);
   const replaceInternal = (errors, err) => hasInternal(errors) ? errors.filter(e => !e.internal).concat(err) : errors;
-  return replaceInternal((error?.graphQLErrors || []).map(e => ({
-    message: e.message,
-    internal: Boolean(!e?.path?.length)
-  })), {message: 'Something went wrong'});
+  return replaceInternal((error?.graphQLErrors || []).map(e => {
+    if (e.extensions?.category) {
+      const validationErrors = e.extensions?.validation;
+      return Object.keys(validationErrors || {}).map(key => validationErrors[key]).flat().map(value => ({message: value, internal: false}));
+    }
+    return {
+      message: e.message,
+      internal: Boolean(!e?.path?.length)
+    }
+  }), {message: 'Something went wrong'}).flat();
 }
