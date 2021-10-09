6891744cdc9d875f4b78d4bcea0263983a56ed55
Russkazkin <russkazkin@gmail.com>
2021-10-09 13:14:41 +0300

    Install and setup vuex store

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import 'alpinejs';
 import Vue from 'vue';
 import apolloProvider from './apollo.config';
 import router from './router.config';
+import store from  "./vuex.config";
 
 import Board from './Board'
 
@@ -13,5 +14,6 @@ Vue.component("Board", Board);
 const app = new Vue({
   el: "#app",
   apolloProvider,
-  router
+  router,
+  store,
 });
